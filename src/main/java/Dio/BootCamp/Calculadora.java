54b7d5cdee01fc920146dc630b0ef010a1bbee73
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-09-17 14:56:22 -0300

    Initial commit

---
+++ src/main/java/Dio/BootCamp/Calculadora.java
@@ -0,0 +1,10 @@
+package Dio.BootCamp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculadora {
+    public int soma(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+}
---
+++ src/test/java/Dio/BootCamp/BootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package Dio.BootCamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
